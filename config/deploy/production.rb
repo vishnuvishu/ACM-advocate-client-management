c649bae44fe6259530951f30ed6345256ca7203f
Athul <athul@hackodezo.com>
2017-08-26 17:20:36 +0530

    hacker

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -49,7 +49,7 @@ namespace :deploy do
    desc 'Restart application'
    task :restart do
       on roles(:app), in: :sequence, wait: 5 do
-         invoke 'puma:start'
+         invoke 'puma:restart'
       end
    end
 
